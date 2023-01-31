3193fafe9b405fd60fc757e72d686ef1dd6d8197
unknown <sajjad.hossan.01234@gmail.com>
2023-01-31 23:23:17 +0600

    watermelon

---
+++ watermelonproblem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int w;
+cin>>w;
+if(w%2==1){
+    cout<<"no"<<endl;
+}
+else if(w==2){
+  cout<<"no"<<endl;
+}
+else{
+    cout<<"yes"<<endl;
+}
+}
+
